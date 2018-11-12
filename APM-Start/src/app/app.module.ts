aadb8231a4860d69dff8f18081eff8bd9d3bb112
Oliver Coulson <oliver.coulson@crownpeak.com>
2018-11-12 16:54:47 +0000

    finished up to module 'Building Nested Components'

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ProductListComponent } from './products/product-list.component';
+import {ConvertToSpacesPipe} from './products/convert-to-spaces.pipe';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ProductListComponent, ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   bootstrap: [AppComponent]
 })
