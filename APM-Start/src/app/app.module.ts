c890b7eaeed018626110f879462d8934ef8a3a64
Oliver Coulson <oliver.coulson@crownpeak.com>
2018-11-14 13:45:31 +0000

    did more stuff

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -7,14 +7,17 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import {ConvertToSpacesPipe} from './products/convert-to-spaces.pipe';
 import {StarComponent} from './shared/star.component';
+import {ProductService} from './products/product.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, HttpClientModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [ProductService]
 })
 export class AppModule { }
