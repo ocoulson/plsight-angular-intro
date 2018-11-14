53416b7cb52fe2b9734ec06cecfd03def7aaeb0a
Oliver Coulson <oliver.coulson@crownpeak.com>
2018-11-14 15:21:11 +0000

    Routes and guards

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,21 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
-import { ProductListComponent } from './products/product-list.component';
+import {ProductListComponent} from './products/product-list.component';
 import {ConvertToSpacesPipe} from './products/convert-to-spaces.pipe';
 import {StarComponent} from './shared/star.component';
 import {ProductService} from './products/product.service';
 import {HttpClientModule} from '@angular/common/http';
+import {ProductDetailComponent} from './products/product-detail.component';
+import {WelcomeComponent} from "./home/welcome.component";
+import {RouterModule} from "@angular/router";
+import {ProductDetailGuard} from "./products/product-detail.guard";
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent],
   providers: [ProductService]
--- APM-Start/src/app/products/product-detail.guard.ts
+++ APM-Start/src/app/products/product-detail.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,16 +7,15 @@ import { Observable } from 'rxjs';
 })
 export class ProductDetailGuard implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    const id = +next.url[1].path;
-    // const id2 = next.paramMap.get('id');
-    // console.log(id2);
-    if (isNaN(id) || id < 1) {
-      alert('Invalid product Id');
+    let id = +next.url[1].path;
+    if(isNaN(id) || id < 1) {
+      alert("Invalid Product id");
       this.router.navigate(['/products']);
       return false;
     }
